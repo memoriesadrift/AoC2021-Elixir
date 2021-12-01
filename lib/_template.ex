b9216cef539562726007eed7907a99748f296c32
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-12-01 08:25:31 +0100

    Add solution template

---
+++ lib/_template.ex
@@ -0,0 +1,26 @@
+defmodule Template do
+  import Reader
+
+  def run1() do
+    solve1()
+    |> format_output
+  end
+
+  def run2() do
+    solve2()
+    |> format_output
+
+  end
+
+  def solve1() do
+    get_input(1)
+  end
+
+  def solve2() do
+    get_input(1)
+  end
+
+  def format_output(output) do
+  end
+
+end
