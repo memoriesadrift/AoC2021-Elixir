1813e38ceb8a35d4772a6f9b568bce5ec6d857bd
Samuel Šulovský <samuel.sulovsky@gmail.com>
2021-12-02 11:33:36 +0100

    Add day 2 task 1

---
+++ lib/day2.ex
@@ -0,0 +1,42 @@
+defmodule Day2 do
+  import Reader
+
+  def run1() do
+    solve1()
+    |> format_output
+  end
+
+  def solve1() do
+    get_input(2)
+    |> parse_input()
+    |> calculate_course([0,0])
+  end
+  def parse_input(input) do
+    Enum.map(input, fn line -> String.split line end)
+    |> Enum.map(fn pair -> [Enum.at(pair, 0), Enum.at(pair, 1) |> String.to_integer] end)
+  end
+
+  def calculate_course([], acc), do: acc
+  def calculate_course([h | t], acc) do
+      command_dir = Enum.at(h, 0)
+      command_speed = Enum.at(h, 1)
+
+      pos = Enum.at(acc, 0)
+      depth = Enum.at(acc, 1)
+
+      cond do
+        command_dir == "forward" ->
+          calculate_course(t, [pos + command_speed, depth])
+        command_dir == "up" ->
+          calculate_course(t, [pos, depth - command_speed])
+        command_dir == "down" ->
+          calculate_course(t, [pos, depth + command_speed])
+      end
+  end
+
+
+  def format_output(output) do
+    "The total forward momentum is #{Enum.at(output, 0)}, the depth is #{Enum.at(output, 1)}, the product is #{Enum.at(output, 0) * Enum.at(output, 1)}"
+  end
+
+end
